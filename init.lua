242f4a564134befaff69115eec40701d3f5eebe8
Djones A. Boni <djboni@gmail.com>
2024-03-07 01:25:14 -0300

    Add spell checking

--- init.lua
+++ init.lua
@@ -158,6 +158,10 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Set spell checking
+vim.opt.spelllang = 'en_us,pt_br'
+vim.opt.spell = true
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })

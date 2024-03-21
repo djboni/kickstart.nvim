493eca65c95080c3521490fe48464b7ab7306ebf
Djones A. Boni <djboni@gmail.com>
2024-03-21 18:18:18 -0300

    Reopen files on the same line (except commit messages)

--- init.lua
+++ init.lua
@@ -886,6 +886,9 @@ require('lazy').setup({
 vim.api.nvim_create_autocmd('BufReadPost', {
   pattern = { '*' },
   callback = function()
+    if string.match(vim.fn.expand '%', '.git/COMMIT_EDITMSG$') then
+      return
+    end
     if vim.fn.line '\'"' > 1 and vim.fn.line '\'"' <= vim.fn.line '$' then
       vim.api.nvim_exec('normal! g\'"', false)
     end

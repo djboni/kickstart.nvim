9d3dfa7e7de343f3567d979a309d450e61b9d6d4
Djones A. Boni <djboni@gmail.com>
2024-05-29 22:51:16 -0300

    Document <leader>t

--- init.lua
+++ init.lua
@@ -325,6 +325,7 @@ require('lazy').setup({
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = 'File [T]ree', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
       }
     end,
